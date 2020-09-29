0b9df00d12666583aab0172a8e9da1fc86908985
JavaHunk2020 <javahunk2020@gmail.com>
2020-09-30 03:35:16 +0530

    creating session when user is authenticated

--- src/main/java/com/cubicit/controller/LoginController.java
+++ src/main/java/com/cubicit/controller/LoginController.java
@@ -1,15 +1,30 @@
 package com.cubicit.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cubicit.controller.vo.CustomerVO;
+import com.cubicit.service.CustomerServiceImpl;
 
 @Controller
 public class LoginController {
 	
-
+	@Autowired
+	private CustomerServiceImpl customerServiceImpl;
+	//EL - EL Implicit Object -13 
+    //${sessionScope.customer.cid}
 	@GetMapping("/customer/dashboard")
-	public String ghome(){
-		return "home";
+	public String ghome(HttpSession session){
+		  Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		  String username=authentication.getName();// javatech1000@gmail.com
+		  CustomerVO customerVO=customerServiceImpl.findByEmail(username);
+		  session.setAttribute("customer", customerVO);
+		  return "home";
 	}
 	
 	//This will execute when u do not have access desire role
